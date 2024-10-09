c8dfddb42c7c313ab44fcc44f44c33ca56b9f80a
Kevil <Kevil@LAPTOP-LNG29E7M>
2024-10-09 16:10:19 +0530

    commit

---
+++ Assignment3/src/Acc/Acc.java
@@ -0,0 +1,36 @@
+package Acc;
+
+import PolicyPolicyImp.PolicyImp;
+public class Acc extends PolicyImp {
+	
+	@Override
+	public double calSal(double salary) {
+		// TODO Auto-generated method stub
+		return salary;
+	}
+	
+	@Override
+	public double calSal(double salary, double bonus) {
+		// TODO Auto-generated method stub
+		return salary+bonus;
+	}
+	
+	@Override
+	public String displayName(String name) {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	
+	@Override
+	public int displayId(int id) {
+		// TODO Auto-generated method stub
+		return id;
+	}
+	
+	@Override
+	public String displayAddr(String addr) {
+		// TODO Auto-generated method stub
+		return addr;
+	}
+
+}
---
+++ Assignment3/src/PolicyPolicyImp/PolicyImp.java
@@ -0,0 +1,26 @@
+package PolicyPolicyImp;
+
+import Policy.Policy;
+
+public abstract  class PolicyImp implements Policy {
+
+	public String displayName(String name) {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	public int displayId(int id) {
+		// TODO Auto-generated method stub
+		return id;
+		
+	}
+
+	public String displayAddr(String addr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
+
